0939c1ae0f5c7c9712b5e74f70cdd7ee64dda701
mahesh <lokilokesh1680@gmail.com>
2023-03-11 18:41:03 +0530

    [lokesh]add:LineComeparison folder is added

---
+++ LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LineComparison
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LineCompoarison");
+        }
+    }
+}
